04fd94225c6d04408f68162d0bfc5ddca2ae0dfb
hemanthr28 <hemanthr@vt.edu>
2023-02-23 22:53:00 -0500

    Updated changes1

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1001,6 +1001,15 @@ asmlinkage long sys_pidfd_send_signal(int pidfd, int sig,
 				       siginfo_t __user *info,
 				       unsigned int flags);
 
+/* Popcorn Linux syscalls */
+#include <popcorn/bundle.h>
+asmlinkage long sys_popcorn_migrate(int nid, void __user * uregs);
+asmlinkage long sys_popcorn_propose_migration(pid_t pid, int nid);
+asmlinkage long sys_popcorn_get_thread_status(struct popcorn_thread_status __user *
+					      status);
+asmlinkage long sys_popcorn_get_node_info(int * _my_nid,
+					  struct popcorn_node_info __user * info);
+
 /*
  * Architecture-specific system calls
  */
@@ -1255,6 +1264,15 @@ ssize_t ksys_pread64(unsigned int fd, char __user *buf, size_t count,
 ssize_t ksys_pwrite64(unsigned int fd, const char __user *buf,
 		      size_t count, loff_t pos);
 int ksys_fallocate(int fd, int mode, loff_t offset, loff_t len);
+#ifdef CONFIG_POPCORN
+long ksys_brk(unsigned long brk);
+long ksys_mremap(unsigned long addr,
+		 unsigned long old_len, unsigned long new_len,
+		 unsigned long flags, unsigned long new_addr);
+long ksys_madvise(unsigned long start, size_t len, int behavior);
+long ksys_mprotect(unsigned long start, size_t len,
+		  unsigned long prot);
+#endif
 #ifdef CONFIG_ADVISE_SYSCALLS
 int ksys_fadvise64_64(int fd, loff_t offset, loff_t len, int advice);
 #else
